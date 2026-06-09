1b609e7439d69ad04741ddfa1dc39867a6cadcf1
omar foobar <medzhidov.omar11@gmail.com>
2026-06-09 15:25:28 +0300

    immutable class

---
+++ ImmutableExample.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// 1. Класс final — запрет наследования
+public final class ImmutableExample {
+
+    public static final class Order {
+        private final int id;
+        // Список товаров — это изменяемый объект! Потокобезопасный код требует защиты.
+        private final List<String> items;
+
+        public Order(int id, List<String> items) {
+            this.id = id;
+
+            // 3. ГЛУБОКОЕ КОПИРОВАНИЕ в конструкторе:
+            // Если мы просто напишем this.items = items, то человек извне сможет изменить
+            // список,
+            // который он передал в конструктор, и наш "неизменяемый" ордер изменится!
+            // Поэтому создаем новый список и оборачиваем в unmodifiableList (запрет на
+            // методы .add/.remove)
+            this.items = Collections.unmodifiableList(new ArrayList<>(items));
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        // 3. ГЛУБОКОЕ КОПИРОВАНИЕ в геттере:
+        // Возвращаем защищенный список, который нельзя модифицировать извне
+        public List<String> getItems() {
+            return items;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> myItems = new ArrayList<>();
+        myItems.add("Laptop");
+        myItems.add("Phone");
+
+        Order order = new Order(1, myItems);
+
+        // Попытка взлома №1: меняем исходный список
+        myItems.add("Juice");
+
+        // Проверяем: изменился ли заказ?
+        System.out.println("Товары в заказе: " + order.getItems());
+        // Выведет только [Laptop, Phone]. Взлом не удался, конструктор защитил данные!
+
+        // Попытка взлома №2: пробуем добавить товар напрямую в геттер
+        order.getItems().add("Shoes"); // ОШИБКА! Вызовет
+        // UnsupportedOperationException в рантайме.
+    }
+}
---
+++ Task2_1.java
@@ -0,0 +1,32 @@
+public class Task2_1 {
+
+    public static class User {
+        public String email;
+
+        public User(String email) {
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) {
+        // User user = new User("test@mail.ru");
+        // User user2 = new User("test@mail.ru");
+
+        // System.out.println(user == user2);
+
+        // System.out.println(user.equals(user2));
+
+        // System.out.println(user.email.equals(user2.email));
+
+        String word1 = "React";
+        String word2 = "React";
+
+        System.out.println(word1 == word2);
+
+        String original = "helloWorld";
+        String part1 = "hello";
+        String part2 = "World";
+        String combined = part1 + part2;
+        System.out.println(original == combined);
+    }
+}
